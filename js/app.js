3983a9740468d6accae8b8982673bdee14016f32
Skeesh24 <d.pacooloff@gmail.com>
2023-05-26 13:59:26 +0500

    fix event name

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let tg = window.Telegram.WebApp
+
+tg.expand()
+
+tg.MainButton.setText("WELCOME")
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
+
+let item = ""
+
+let card_1 = document.getElementsByClassName("card-1")[0]
+let card_6 = document.getElementByClassName("card-6")[0]
+let card_12 = document.getElementByClassName("card-12")[0]
+
+function chooseSub(sub) {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide()
+  } else {
+    tg.MainButton.setText(`U take sub for ${sub} month`)
+    item = sub
+    tg.MainButton.show()
+  }
+}
+
+card_1.addEventListener("click", () => {
+  chooseSub(1)
+})
+
+card_6.addEventListener("click", () => {
+  chooseSub(6)
+})
+
+card_12.addEventListener("click", () => {
+  chooseSub(12)
+})
+
+Telegram.WebApp.onEvent("mainButtonClicked", () => {
+  tg.sendData(item)
+})
